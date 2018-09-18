de291afd087328b5f33f0b3582b6d1dce1fb84c6
Jazz Ng <jazzng.js@gmail.com>
2018-09-18 23:07:41 +0800

    v1.0.12
    Validation done for
    register cashier
    register new stock
    stock in / out

--- ejcloset/adminregistercashier.aspx.designer.cs
+++ ejcloset/adminregistercashier.aspx.designer.cs
@@ -76,6 +76,15 @@ namespace ejcloset {
         protected global::System.Web.UI.HtmlControls.HtmlInputPassword txt_user_password2;
         
         /// <summary>
+        /// btn_clear control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn_clear;
+        
+        /// <summary>
         /// btn_register control.
         /// </summary>
         /// <remarks>
--- ejcloset/adminregisternewstock.aspx.cs
+++ ejcloset/adminregisternewstock.aspx.cs
@@ -35,21 +35,62 @@ namespace ejcloset
 
         private void registeritem()
         {
-            con.Open();
+            if (ItemExistVerify(txt_item_code.Value.ToString()) == true)
+            {
+                if (DecimalVerify(txt_item_price.Value.ToString()) == true)
+                {
+                    con.Open();
+                    try
+                    {
+                        string query = "INSERT INTO Inventories (item_code, item_title, item_price, item_category, item_supplier, item_quantity) values (@item_code, @item_title, @item_price, @item_category, @item_supplier, @item_quantity)";
+
+                        SqlCommand cmd = new SqlCommand(query, con);
+                        cmd.Parameters.AddWithValue("@item_code", txt_item_code.Value.ToString());
+                        cmd.Parameters.AddWithValue("@item_title", txt_item_title.Value.ToString());
+                        cmd.Parameters.AddWithValue("@item_price", Math.Round(Convert.ToDecimal(txt_item_price.Value.ToString()), 2));
+                        cmd.Parameters.AddWithValue("@item_category", ddl_item_category.Value.ToString());
+                        cmd.Parameters.AddWithValue("@item_supplier", txt_item_supplier.Value.ToString());
+                        cmd.Parameters.AddWithValue("@item_quantity", txt_item_quantity.Value.ToString());
+
+                        cmd.ExecuteNonQuery();
+                        Response.Write("<script type='text/javascript'>alert('Register Successful');window.location='adminregisternewstock.aspx';</script>");
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Response.Write("Error: " + ex.ToString());
+                    }
+                    con.Close();
+                }
+                else
+                {
+                    Response.Write("<script type='text/javascript'>alert('Invalid Price');</script>");
+                }
+
+            }
+            else
+            {
+                Response.Write("<script type='text/javascript'>alert('Item Code Already Existed');</script>");
+            }
+            
+        }
+
+        private Boolean ItemExistVerify(String itemcode)
+        {
+            Boolean verify = false;
             try
             {
-                string query = "INSERT INTO Inventories (item_code, item_title, item_price, item_category, item_supplier, item_quantity) values (@item_code, @item_title, @item_price, @item_category, @item_supplier, @item_quantity)";
-
-                SqlCommand cmd = new SqlCommand(query, con);
-                cmd.Parameters.AddWithValue("@item_code", txt_item_code.Value.ToString());
-                cmd.Parameters.AddWithValue("@item_title", txt_item_title.Value.ToString());
-                cmd.Parameters.AddWithValue("@item_price", txt_item_price.Value.ToString());
-                cmd.Parameters.AddWithValue("@item_category", ddl_item_category.Value.ToString());
-                cmd.Parameters.AddWithValue("@item_supplier", txt_item_supplier.Value.ToString());
-                cmd.Parameters.AddWithValue("@item_quantity", txt_item_quantity.Value.ToString());
-
-                cmd.ExecuteNonQuery();
-                Response.Write("<script type='text/javascript'>alert('Register Successful');window.location='adminregisternewstock.aspx';</script>");
+                con.Open();
+                SqlCommand cmd_role = new SqlCommand("select count(*) from Inventories where item_code = '" + itemcode + "'", con);
+                int count = Convert.ToInt32(cmd_role.ExecuteScalar().ToString());
+                if (count > 0)
+                {
+                    verify = false;
+                }
+                else
+                {
+                    verify = true;
+                }
 
             }
             catch (Exception ex)
@@ -57,6 +98,27 @@ namespace ejcloset
                 Response.Write("Error: " + ex.ToString());
             }
             con.Close();
+
+            return verify;
+        }
+
+        private Boolean DecimalVerify(String price)
+        {
+            Boolean verify = false;
+            foreach (char ch in price)
+            {
+                if (!char.IsDigit(ch) && ch != '.')
+                {
+                    verify = false;
+                }
+                else
+                {
+                    verify = true;
+                }
+
+            }
+
+            return verify;
         }
     }
 }
